36bbefa035990f579a401ac14710dba79819df0d
Arthur Saito <arthur_saito1998@hotmail.com>
2023-07-31 20:55:45 -0300

    feat: add service methods on courseService

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -18,27 +18,26 @@ export class CoursesController {
 
   @Get()
   findAll(@Res() response) {
-    return response.status(HttpStatus.OK).send('List all courses');
+    return this.coursesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `return #${id} course`;
+    return this.coursesService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
-    return body;
+    return this.coursesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `Update course ${id}`;
+    return this.coursesService.uptade(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `Remove #${id} course`;
+    return this.coursesService.remove(id);
   }
 }
