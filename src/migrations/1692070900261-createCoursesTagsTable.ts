7ce934874e66d8e78aa2db3376b667e6cb4e2757
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-15 00:48:23 -0300

    feat: adding a createCoursesTagsTable migrations

---
+++ src/migrations/1692070900261-createCoursesTagsTable.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCoursesTagsTable1692070900261 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'courses_tags',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generated_v4()',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('courses_tags');
+  }
+}
