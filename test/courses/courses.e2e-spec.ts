f91ed584f6431871cf332d3d573150876bec520f
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-21 23:55:01 -0300

    feat: adding a course e2e test

---
+++ test/courses/courses.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import * as request from 'supertest';
+
+import { CoursesModule } from '../../src/courses/courses.module';
+
+describe('Courses: /courses', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [CoursesModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+  it.todo('Create POST /courses');
+});
